25ddcf4b4eb9e0f2fe7a1dfe881213b17a37fc82
Kovid Kunduru <kovid@Kovids-MacBook-Air.local>
2024-06-20 21:39:05 +1000

    Schedule Rectifier Done with React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,77 @@
+import { useState } from "react";
 import "./App.css";
+import { Form } from "./components/Form";
+import { Table } from "./components/Table";
+
+const hoursPerWeek = 24 * 7;
 
 function App() {
-  return <>Hello World guru</>;
+  const [taskList, setTaskList] = useState([]);
+  const ttlHr = taskList.reduce((acc, item) => {
+    return acc + +item.hr;
+  }, 0);
+  const addTaskList = (taskObj) => {
+    const obj = {
+      ...taskObj,
+      id: randomIdGenerator(),
+      type: "entry",
+    };
+    if (ttlHr + +taskObj.hr > hoursPerWeek) {
+      return alert("Sorry Bhayya. You are exceeding, hours per week");
+    }
+    setTaskList([...taskList, obj]);
+  };
+
+  const switchTask = (id, type) => {
+    setTaskList(
+      taskList.map((item) => {
+        if (item.id === id) {
+          item.type = type;
+        }
+        return item;
+      })
+    );
+  };
+  const randomIdGenerator = (length = 6) => {
+    const str =
+      "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAMNBVCXZ1234567890";
+    let id = "";
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * str.length);
+      id += str[randomIndex];
+    }
+    return id;
+  };
+
+  const handleOnDelete = (id) => {
+    // console.log(id);
+    if (window.confirm("Are you sure, you want to delete this?")) {
+      setTaskList(taskList.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <div className="wrapper pt-5">
+      {/* <!-- title  --> */}
+      <div className="container">
+        <h1 className="text-center">Not To Do List</h1>
+
+        {/* <!-- form  --> */}
+        <Form addTaskList={addTaskList} />
+
+        {/* <!-- tables --> */}
+
+        <Table
+          taskList={taskList}
+          switchTask={switchTask}
+          handleOnDelete={handleOnDelete}
+        />
+        <div className="alert alert-success">
+          The total hours allocated = <span id="ttlHrs">{ttlHr}</span> hrs
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
